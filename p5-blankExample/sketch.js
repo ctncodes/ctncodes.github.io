d6c294cc390aed423b596816cb50057073b5ae64
ctncodes <35818754+ctncodes@users.noreply.github.com>
2018-03-08 12:45:45 -0800

    I added these files

---
+++ p5-blankExample/sketch.js
@@ -0,0 +1,17 @@
+
+// declare variables here
+
+
+// setup runs once
+function setup() {
+	createCanvas(500, 500);
+	background(150);
+}
+
+// draw loops and loops
+function draw() {
+
+}
+
+
+// write functions here
