78f3a98d3cacefd50ecd01f7e510a0de303f1723
ctncodes <35818754+ctncodes@users.noreply.github.com>
2018-03-22 12:12:05 -0700

    Update sketch.js

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,17 +1,35 @@
+let clouds;
+let x = 0;
 
-// declare variables here
+function preload() {
+  // you will need to change this to the file path to your image
+  clouds = loadImage('assets/superMarioClouds_1.png');
+}
 
-
-// setup runs once
 function setup() {
-	createCanvas(500, 500);
-	background(150);
-}
+  createCanvas(500, 500);
+  background(200);
 
-// draw loops and loops
+  // Top-left corner of the img is at (0, 0)
+  // Width and height are the img's original width and height
+  // image(img, 0, 0);
+
+  // If you want to scale the image to 100 x 100 pixels
+  // image(img, 0, 0, 100, 100);
+
+  // If you want to scale image by 50%
+
+}
 function draw() {
-
+    background(200);
+  image(clouds, x, 10, clouds.width/2, clouds.height/2);
+  image(clouds, x, 100, clouds.width/2, clouds.height/2);
+  x++;
+  }
+  // while (x < width) {
+  //   fill(129, 206, 15);
+  //   rect(x, 100, 10, 10);
+  //   x += 20;
+  //   image(img, 0, 0);
+  // }
 }
-
-
-// write functions here
