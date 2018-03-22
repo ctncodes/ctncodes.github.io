6807333bd5f23e26c7fa560dabe3e96fa099a611
ctncodes <35818754+ctncodes@users.noreply.github.com>
2018-03-22 14:02:39 -0700

    Update sketch.js

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,30 +1,33 @@
-let clouds;
+let candy;
 let x = 0;
-
+let y = 0;
+let speed = 5;
 function preload() {
   // you will need to change this to the file path to your image
-  clouds = loadImage('assets/superMarioClouds_1.png');
+  candy = loadImage('assets/Candy of all sorts.png');
 }
-
 function setup() {
   createCanvas(500, 500);
   background(200);
-
   // Top-left corner of the img is at (0, 0)
   // Width and height are the img's original width and height
   // image(img, 0, 0);
-
   // If you want to scale the image to 100 x 100 pixels
   // image(img, 0, 0, 100, 100);
-
   // If you want to scale image by 50%
-
 }
 function draw() {
-    background(200);
-  image(clouds, x, 10, clouds.width/2, clouds.height/2);
-  image(clouds, x, 100, clouds.width/2, clouds.height/2);
-  x++;
+  background(200);
+  image(candy, 0, y, candy.width, candy.height);
+  image(candy, candy.width, y, candy.width, candy.height);
+  image(candy, candy.width, y + candy.height, candy.width, candy.height);
+  image(candy, 0, y + candy.height, candy.width, candy.height);
+  move();
+}
+function move() {
+  y += speed;
+  if (y >= width || y < -225) {
+    speed = -speed;
   }
   // while (x < width) {
   //   fill(129, 206, 15);
